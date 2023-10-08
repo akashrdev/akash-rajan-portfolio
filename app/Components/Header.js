ace0b1ce8672fdb74669169aaf82a1c8d1a791df
Akash Rajan <akashrajan93@gmail.com>
2023-10-07 22:29:24 -0500

    adjusted header spacing for larger screens

--- app/Components/Header.js
+++ app/Components/Header.js
@@ -2,9 +2,9 @@ import '../../app/globals.css';
 
 const Header = () => {
   return (
-    <div className="w-screen flex flex-row justify-between md:p-5 pl-2 md:pl-16">
+    <div className="w-screen flex flex-row justify-between pl-2 md:pl-16 md:pt-2">
       <a href="#">
-        <h1 className="md:text-4xl text-4xl font-bold text-white font-spacemono ">
+        <h1 className="md:text-5xl text-4xl font-bold text-white font-spacemono ">
           Akash Rajan
         </h1>
       </a>
