a444c7de23133b18c5a507330df0481271e76d77
Akash Rajan <akashrajan93@gmail.com>
2023-10-08 08:54:37 -0500

    updated breakpoints for better responsiveness

--- app/Components/ProjectList.js
+++ app/Components/ProjectList.js
@@ -6,13 +6,13 @@ import PHP from './Projects/PHP.js';
 const ProjectList = () => {
   return (
     <div>
-      <div className="flex flex-col md:flex-row space-y-4 md:space-x-6 md:space-y-0 align-middle justify-center">
+      <div className="flex flex-col lg:flex-row space-y-4 lg:space-x-6 lg:space-y-0 align-middle justify-center">
         <div className="flex flex-row space-x-4 md:space-x-6 justify-center">
           <WiskyBusiness />
           <Aficionado />
         </div>
 
-        <div className="flex flex-row space-x-4 md:space-x-6 justify-center">
+        <div className="flex flex-row space-x-4 lg:space-x-6 justify-center">
           <Atelier />
           <PHP />
         </div>
--- app/Components/TechStack.js
+++ app/Components/TechStack.js
@@ -3,49 +3,49 @@ const TechStack = () => {
     <div className="flex flex-col border border-indigo-800 w-screen p-2 hidden md:block md:mt-10">
       <h1 className="flex text-4xl font-spacemono justify-center items-center text-white font-"></h1>
 
-      <div className="relative flex justify-center flex-row md:space-x-16 space-x-5 align-middle">
+      <div className="relative flex justify-center flex-row sm:space-x-10 lg:space-x-16 space-x-5 align-middle">
         <img
-          className="md:h-16 md:w-16 w-8"
+          className="sm:h-12 sm:w-12 lg:h-16 lg:w-16 w-8 "
           src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-plain.svg"
         />
 
         <img
-          className="md:h-16 md:w-16 w-8"
+          className="sm:h-12 sm:w-12 lg:h-16 lg:w-16 w-8 "
           src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original-wordmark.svg"
         />
 
         <img
-          className="md:h-16 md:w-16 w-8"
+          className="sm:h-12 sm:w-12 lg:h-16 lg:w-16 w-8 "
           src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nodejs/nodejs-original.svg"
         />
 
         <img
-          className="md:h-16 md:w-16 w-8"
+          className="sm:h-12 sm:w-12 lg:h-16 lg:w-16 w-8 "
           src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/express/express-original.svg"
         />
 
         <img
-          className="md:h-16 md:w-16 w-8"
+          className="sm:h-12 sm:w-12 lg:h-16 lg:w-16 w-8 "
           src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/postgresql/postgresql-plain-wordmark.svg"
         />
 
         <img
-          className="md:h-16 md:w-16 w-8"
+          className="sm:h-12 sm:w-12 lg:h-16 lg:w-16 w-8"
           src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/mongodb/mongodb-plain-wordmark.svg"
         />
 
         <img
-          className="md:h-16 md:w-16 w-8"
+          className="sm:h-12 sm:w-12 lg:h-16 lg:w-16 w-8"
           src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/docker/docker-plain-wordmark.svg"
         />
 
         <img
-          className="md:h-16 md:w-16 w-8"
+          className="sm:h-12 sm:w-12 lg:h-16 lg:w-16 w-8"
           src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/amazonwebservices/amazonwebservices-plain-wordmark.svg"
         />
 
         <img
-          className="md:h-16 md:w-16 w-8 text-white"
+          className="sm:h-12 sm:w-12 lg:h-16 lg:w-16 w-8 text-white"
           src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nextjs/nextjs-original-wordmark.svg"
         />
       </div>
